27c4631d7e3aef11ca81bbfd9bcd3a2e920eeba3
haiyang5210 <haiyang5210@gmail.com>
2015-01-29 00:34:35 +0800

    Signed-off-by: haiyang5210 <haiyang5210@gmail.com>

--- cmd/mongo.bat
+++ cmd/mongo.bat
@@ -1,4 +1,31 @@
+cd .
+@echo off
+echo             .==========. 
+echo        ."==^            ^==". 
+echo    "==^                      ^==". 
+echo   "            MongoDB            "  
+echo  "\.                             ./" 
+echo  "  ^===.                   .===^  " 
+echo  "       ^=================^       " 
+echo  "                                 " 
+echo  "                                 " 
+echo  "\.                             ./" 
+echo  "  ^===.                   .===^  " 
+echo  "       ^=================^       " 
+echo  "                                 " 
+echo  "                                 " 
+echo  "\.                             ./" 
+echo  "  ^===.                   .===^  " 
+echo  "       ^=================^       " 
+echo  "                                 " 
+echo  "                                 " 
+echo  "\.                             ./" 
+echo     ^===.                   .===^    
+echo          ^=================^         
+echo on  
 @echo off
 cd C:\
 @echo on
-mongodb2.4.10\bin\mongod.exe -f C:\mongodb\mongo.conf
+C:\mongodb2.4.10\bin\mongod.exe -f C:\mongodb\mongo.conf
+PAUSE
+
