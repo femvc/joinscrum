6e17b1cfc6f14a073eb5c6ae6679c85bad81019a
wanghaiyang@hujiang.com <wanghaiyang@hujiang.com>
2015-01-29 14:23:17 +0800

    Signed-off-by: wanghaiyang@hujiang.com <wanghaiyang@hujiang.com>

--- cmd/mongo.bat
+++ cmd/mongo.bat
@@ -1,30 +1,27 @@
 cd .
 @echo off
-echo             ___________
-echo       ____.^^           ^^._____
-echo   .--`                        '-._
-echo  (            MongoDB             )
-echo  ^|`--.____                ____.--'^|
-echo  ^|        `-.__________.-'        ^|
-echo  ^|                                ^|
-echo  ^|                                ^|
+echo  ^     ____.-------------.____ 
+echo  ^ .--'      +-+-+++-+-+      '--._
+echo  ^(          ^| MongoDB ^|           )
+echo  ^|`--.____  +-+-+++-+-+   ____.--'^|
+echo  ^|        `-.          .-'        ^|
+echo  ^|           £þ£þ£þ£þ£þ           ^|
 echo  ^|                                ^|
 echo  ^|`--.____                ____.--'^|
-echo  ^|        `-.__________.-'        ^|
-echo  ^|                                ^|
-echo  ^|                                ^|
+echo  ^|        `-.          .-'        ^|
+echo  ^|           £þ£þ£þ£þ£þ           ^|
 echo  ^|                                ^|
 echo  ^|`--.____                ____.--'^|
-echo  ^|        `-.__________.-'        ^|
-echo  ^|                                ^|
-echo  ^|                                ^|
+echo  ^|        `-.          .-'        ^|
+echo  ^|           £þ£þ£þ£þ£þ           ^|
 echo  ^(                                )
 echo  ^ `--.____                ____.--'
-echo  ^         `-.__________.-'        
+echo  ^         `-.          .-'        
+echo  ^            £þ£þ£þ£þ£þ
 echo on  
 @echo off
 cd C:\
 @echo on
 C:\mongodb2.4.10\bin\mongod.exe -f C:\mongodb\mongo.conf
-PAUSE
+::PAUSE
 
